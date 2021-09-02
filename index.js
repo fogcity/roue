09647a8c4bf95f91c72c67b0bee88bd88ca201b9
Dylan <583128870@qq.com>
2021-09-02 18:37:26 +0800

    Uninstall commander & import minimist prompts .etc

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
 #! /usr/bin/env node
-const { program } = require("commander");
-const list = require("./commands/list");
-const add = require("./commands/add");
-const markDone = require("./commands/markDone");
-program.command("list").description("List all the TODO tasks").action(list);
-program.command("add <task>").description("Add a new TODO task").action(add);
-program
-  .command("mark-done")
-  .description("Mark commands done")
-  .option(
-    "-t, --tasks <tasks...>",
-    "The tasks to mark done. If not specified, all tasks will be marked done."
-  )
-  .action(markDone);
-program.parse();
+import prompts from "prompts";
+import parseArgv from "minimist";
+import fs from "fs-extra";
+import ora from "ora";
+const argv = parseArgv(process.argv.slice(2));
+{
+  (async () => {
+    if (argv._.length != 0) {
+      const command = argv._[0];
+      const libName = argv._[1];
+      if (command == "create" && libName) {
+        const template = argv.template;
+
+        const questions = [
+          {
+            type: "confirm",
+            name: "needEslint",
+            message: "Can you need eslint?",
+            active: "yes",
+          },
+          {
+            type: "confirm",
+            name: "needPrettier",
+            message: "Can you need prettier?",
+          },
+          {
+            ...(template || {
+              type: "select",
+              name: "platform",
+              message: "Which platform do you want to build a library for",
+              choices: [
+                {
+                  title: "React",
+                  value: "React",
+                },
+              ],
+            }),
+          },
+        ];
+
+        const config = await prompts(questions);
+        const spinner = ora("create files...").start();
+        try {
+          await fs.ensureDir(libName);
+          spinner.succeed("success!");
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+  })();
+}
